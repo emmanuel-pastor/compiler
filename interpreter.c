3fec936df9b6e26fb129d159569467f3cafb8162
Emmanuel Pastor <ommanu13@hotmail.fr>
2022-05-31 00:30:30 +0200

    [BUGFIX] Put interpreter function declarations before they are called

--- interpreter.c
+++ interpreter.c
@@ -8,70 +8,6 @@ int mem_table[MAX_MEM];
 AsmInst asm_table[MAX_ASM_INST];
 int i = 0;
 
-void launch_interpreter(int nb_inst)
-{
-    while (i < nb_inst)
-    {
-        switch (asm_table[i].operator)
-        {
-        case 0:
-            add(asm_table[i].op1, asm_table[i].op2, asm_table[i].op3);
-            i++;
-            break;
-        case 1:
-            mul(asm_table[i].op1, asm_table[i].op2, asm_table[i].op3);
-            i++;
-            break;
-        case 2:
-            sou(asm_table[i].op1, asm_table[i].op2, asm_table[i].op3);
-            i++;
-            break;
-        case 3:
-            div_asm(asm_table[i].op1, asm_table[i].op2, asm_table[i].op3);
-            i++;
-            break;
-        case 4:
-            cop(asm_table[i].op1, asm_table[i].op2);
-            i++;
-            break;
-        case 5:
-            afc(asm_table[i].op1, asm_table[i].op2);
-            i++;
-            break;
-        case 6:
-            jmp(asm_table[i].op1);
-            break;
-        case 7:
-            jmf(asm_table[i].op1, asm_table[i].op2);
-            break;
-        case 8:
-            inf(asm_table[i].op1, asm_table[i].op2, asm_table[i].op3);
-            i++;
-            break;
-        case 9:
-            sup(asm_table[i].op1, asm_table[i].op2, asm_table[i].op3);
-            i++;
-            break;
-        case 10:
-            equ(asm_table[i].op1, asm_table[i].op2, asm_table[i].op3);
-            i++;
-            break;
-        case 11:
-            pri(asm_table[i].op1);
-            i++;
-            break;
-        case 12:
-            and(asm_table[i].op1, asm_table[i].op2, asm_table[i].op3);
-            i++;
-            break;
-        case 13:
-            or(asm_table[i].op1, asm_table[i].op2, asm_table[i].op3);
-            i++;
-            break;
-        }
-    }
-}
-
 void add(int op1, int op2, int op3)
 {
     mem_table[op1] = mem_table[op2] + mem_table[op3];
@@ -147,4 +83,68 @@ void and(int op1, int op2, int op3)
 void or(int op1, int op2, int op3)
 {
     mem_table[op1] = mem_table[op2] || mem_table[op3];
+}
+
+void launch_interpreter(int nb_inst)
+{
+    while (i < nb_inst)
+    {
+        switch (asm_table[i].operator)
+        {
+        case 0:
+            add(asm_table[i].op1, asm_table[i].op2, asm_table[i].op3);
+            i++;
+            break;
+        case 1:
+            mul(asm_table[i].op1, asm_table[i].op2, asm_table[i].op3);
+            i++;
+            break;
+        case 2:
+            sou(asm_table[i].op1, asm_table[i].op2, asm_table[i].op3);
+            i++;
+            break;
+        case 3:
+            div_asm(asm_table[i].op1, asm_table[i].op2, asm_table[i].op3);
+            i++;
+            break;
+        case 4:
+            cop(asm_table[i].op1, asm_table[i].op2);
+            i++;
+            break;
+        case 5:
+            afc(asm_table[i].op1, asm_table[i].op2);
+            i++;
+            break;
+        case 6:
+            jmp(asm_table[i].op1);
+            break;
+        case 7:
+            jmf(asm_table[i].op1, asm_table[i].op2);
+            break;
+        case 8:
+            inf(asm_table[i].op1, asm_table[i].op2, asm_table[i].op3);
+            i++;
+            break;
+        case 9:
+            sup(asm_table[i].op1, asm_table[i].op2, asm_table[i].op3);
+            i++;
+            break;
+        case 10:
+            equ(asm_table[i].op1, asm_table[i].op2, asm_table[i].op3);
+            i++;
+            break;
+        case 11:
+            pri(asm_table[i].op1);
+            i++;
+            break;
+        case 12:
+            and(asm_table[i].op1, asm_table[i].op2, asm_table[i].op3);
+            i++;
+            break;
+        case 13:
+            or(asm_table[i].op1, asm_table[i].op2, asm_table[i].op3);
+            i++;
+            break;
+        }
+    }
 }
